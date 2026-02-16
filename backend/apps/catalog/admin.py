55a924e6e61fc3cdc0625d9897cd814afd261ce4
WiZY991 <anton_tenditnik11@mail.ru>
2026-02-16 23:39:57 +1000

    fix rotation 3d model and import 1 tabl id

--- backend/apps/catalog/admin.py
+++ backend/apps/catalog/admin.py
@@ -28,8 +28,9 @@ from apps.admin_utils import ExportExcelMixin
 
 @admin.register(Category)
 class CategoryAdmin(SortableAdminMixin, ExportExcelMixin, admin.ModelAdmin):
-    list_display = ("id", "name", "slug", "parent", "preview_image")
+    list_display = ("order", "id", "name", "slug", "parent", "preview_image")
     list_display_links = ("name",)
+    list_editable = ("order",)  # можно менять порядок вручную, если drag не работает
     search_fields = ("name",)
     prepopulated_fields = {"slug": ("name",)}
     actions = ["export_selected_to_excel"]
